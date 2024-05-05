6c0eceb7d4f4e640e4b5b2242c5d13143cd31421
vietpd@tksvietnam.com <vietpd@tksvietnam.com>
2024-05-05 17:33:38 +0700

    Fix: update css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import './card.css'
+import React from "react";
+import "./card.css";
 
-// const useStyles = makeStyles({
-
-// });
-const Card = ({title, description, link, img}) => {
-  // const classes = useStyles();
-    return (
-        <a href={link}>
-          <div className="card-container">
-            <div className="card">
-              <div className="img-content">
-                <img src={img} alt="" style={{width: '100%', height: '100%'}}/>
-              </div>
-              <div className="content">
-                <p className="heading">{title}</p>
-                <p>{description}</p>
-              </div>
-            </div>
+const Card = ({ title, description, link, img }) => {
+  return (
+    <div className="card-container">
+      <a href={link} className="">
+        <div className="card">
+          <div className="img-content">
+            <img src={img} alt="" style={{ width: "100%", height: "100%" }} />
           </div>
-        </a>
-    );
+          <div className="content">
+            <p className="heading">{title}</p>
+            <p>{description}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 };
 
-export default Card
+export default Card;
