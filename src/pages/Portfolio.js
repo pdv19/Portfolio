b663aa27c4327dc833bd467a749f0b163eaeef4d
vietpd@tksvietnam.com <vietpd@tksvietnam.com>
2024-06-16 23:33:48 +0700

    update portfolio img

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,77 +11,76 @@ import { motion } from "framer-motion";
 //import transition
 import { transition1 } from "../transitions";
 import { CursorContext } from "../context/CursorContext";
+import Card from "../components/Card";
+import { PortfolioSourceImg } from "../components/LazyImage/data";
+import ContactMeBtn from "../components/ContactMeBtn";
 
 const Portfolio = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
   return (
-    <motion.section
-      initial={{ opacity: 0, y: "100%" }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: "100%" }}
-      transition={transition1}
-      className="section"
-      style={{backgroundColor: '#F1F2F2'}}
-    >
-      <div className="container mx-auto h-full relative">
-        <div className="flex flex-col lg:flex-row h-full items-center jusify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8">
-          {/* text */}
-          <motion.div
-            onMouseEnter={mouseEnterHandler}
-            onMouseLeave={mouseLeaveHandler}
-            initial={{ opacity: 0, y: "-80%" }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: "-80%" }}
-            transition={transition1}
-            className="flex flex-col lg:items-start"
-          >
-            <h1 className="h1">Portfolio</h1>
-            <p className="mb-12 max-w-sm">
-              My name is <b>Ethan Thuan Le</b>
-              <br />
-            </p>
-            <Link to={"/contact"} className="btn mb-[30px] mx-auto lg:mx-0">
-              Contact me
-            </Link>
-          </motion.div>
-          {/* images grid */}
-          <div
-            onMouseEnter={mouseEnterHandler}
-            onMouseLeave={mouseLeaveHandler}
-            className="grid grid-cols-2 lg:gap-2"
-          >
-            <div className="max-w-[250px] lg:max-w=[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img
-                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
-                src={Image1}
-                alt=""
-              />
-            </div>
-            <div className="max-w-[250px] lg:max-w=[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img
-                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
-                src={Image2}
-                alt=""
-              />
-            </div>
-            <div className="max-w-[250px] lg:max-w=[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img
-                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
-                src={Image3}
-                alt=""
-              />
-            </div>
-            <div className="max-w-[250px] lg:max-w=[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
-              <img
-                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
-                src={Image4}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </motion.section>
+    <div>
+      <motion.section
+        initial={{ opacity: 0, y: "100%" }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: "100%" }}
+        transition={transition1}
+        className="section text-center"
+        style={{ backgroundColor: "#F1F2F2" }}
+      >
+        {/* <div className="container mx-auto h-full relative"> */}
+        {/* <div className="flex flex-col lg:flex-row h-full items-center justify-center lg:justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8"> */}
+        <motion.div
+          style={{ paddingTop: "10rem" }}
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+          initial={{ opacity: 0, y: "-80%" }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: "-80%" }}
+          transition={transition1}
+          className="flex flex-col lg:items-start mx-auto mt-auto"
+        >
+          <h1 className="h1 mx-auto mt-auto">Portfolio</h1>
+          <p className="mb-12 max-w-sm  mx-auto mt-auto">
+            <b>Ethan Thuan Le</b>
+            <br />
+            {/* <br /> */}
+            Project
+          </p>
+          {/* <Link to={"/contact"} className="btn mb-[30px] mx-auto mx-auto mt-auto">
+                Contact me
+              </Link> */}
+        </motion.div>
+        {/* </div> */}
+        {/* </div> */}
+        <motion.div className="container justify-items-center mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-20">
+          <Card
+            title="Portraiture"
+            description=""
+            link="/portfolio/portraiture"
+            img={PortfolioSourceImg.SHOT_1}
+          />
+          <Card
+            title="Editorial"
+            description=""
+            link="/portfolio/editorial"
+            img={PortfolioSourceImg.SHOT_2}
+          />
+          <Card
+            title="Commercial"
+            description=""
+            link="/portfolio/commercial"
+            img={PortfolioSourceImg.SHOT_3}
+          />
+          <Card
+            title="Film"
+            description=""
+            link="/portfolio/video"
+            img={PortfolioSourceImg.SHOT_4}
+          />
+        </motion.div>
+        <ContactMeBtn extraClassName="mx-auto my-auto" linkTo={"/contact"} />
+      </motion.section>
+    </div>
   );
 };
 
